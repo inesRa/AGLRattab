d731110bef9e4065a5acf3ff79d2be4f3d06f3d6
ines <rattabines@gmail.com>
2018-12-25 11:50:31 +0100

    change 1 of hello.java

--- AglProject/src/ma/emi/Rattab/Hello.java
+++ AglProject/src/ma/emi/Rattab/Hello.java
@@ -3,7 +3,7 @@ package ma.emi.Rattab;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello word 1");
+		System.out.println("hello word from ines");
 
 	}
 
